50d6f91a59a606242577c32337cead8ce693f71f
yoones rahimian <codeloom.biz@gmail.com>
2026-05-19 23:35:02 +0330

    make posts, post_details, new_post page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from core.views import say_hello
+from core.views import say_hello, post_list, post_details, new_post
 
 urlpatterns = [
-    path('hello/', say_hello)
+    path('hello/', say_hello),
+    path('posts/', post_list, name='post_list'),
+    path('post/<post_id>/', post_details, name='post_details'),
+    path('new/post/', new_post, name='new_post'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,16 @@ def say_hello (request):
     posts = Post.objects.all()
     context = {'posts':posts}
     return render(request, 'index.html', context=context)
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request, 'index.html', context=context)
+
+def post_details(request, post_id):
+    post = Post.objects.filter(id=post_id).first()
+    context = {'post':post}
+    return render(request, 'post_details.html', context=context)
+
+def new_post(request):
+    return render(request, 'new_post.html')
